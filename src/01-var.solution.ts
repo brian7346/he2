0b512535619e90e6991c8e98807815730ae2039b
brian7346 <doctorbrian97@gmail.com>
2025-05-21 23:04:29 +0400

    first commit

---
+++ src/01-var.solution.ts
@@ -0,0 +1,15 @@
+/*
+ * @metadata {
+ *   "title": "Var",
+ *   "descriptionVideoId": "",
+ *   "solutionVideoId": ""
+ * }
+ */
+
+// Ваше задание здесь
+// Опишите что нужно сделать
+
+export const solution() {
+  // Здесь должно быть решение
+  return true;
+}
---
+++ src/02-alias.solution.ts
@@ -0,0 +1,15 @@
+/*
+ * @metadata {
+ *   "title": "Alias",
+ *   "descriptionVideoId": "",
+ *   "solutionVideoId": ""
+ * }
+ */
+
+// Ваше задание здесь
+// Опишите что нужно сделать
+
+export const solution() {
+  // Здесь должно быть решение
+  return true;
+}
